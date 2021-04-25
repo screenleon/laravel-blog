1ff227fb1b6d2a88f66799304a9de6c3779f8a0c
screenleon <screen.leon@gmail.com>
2021-04-25 17:27:06 +0800

    Adjust nav button, clear unremove file, update css file

--- resources/views/components/nav-layout.blade.php
+++ resources/views/components/nav-layout.blade.php
@@ -7,6 +7,27 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item{{ Request::url() == route('index') ? ' active' : '' }}">
+                    <a class="nav-link" href="{{ route('index') }}">Home
+                        @if (Request::url() == route('index'))
+                            <span class="sr-only">(current)</span>
+                        @endif
+                    </a>
+                </li>
+                <li class="nav-item{{ Request::url() == route('posts.index') ? ' active' : '' }}">
+                    <a class="nav-link" href="route('posts.index')">Posts</a>
+                    @if (Request::url() == route('index'))
+                        <span class="sr-only">(current)</span>
+                    @endif
+                </li>
+                <li class="nav-item dropdown">
+                    <a href="#" id="toolsDropdown" class="nav-link dropdown-toggle uppercase" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ __('Tools')}}
+                    </a>
+                </li>
+            </ul>
             @guest
                 @if (Route::has('login'))
                     <li class="nav-item">
@@ -21,14 +42,14 @@
                 @endif
             @else
                 <li class="nav-item dropdown">
-                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }}
                     </a>
 
                     <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="{{ route('logout') }}"
-                           onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
                             {{ __('Logout') }}
                         </a>
 
