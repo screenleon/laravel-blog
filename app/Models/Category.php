273b96f9cbd520504bdd9af64341d137fafa0db4
screenleon <screen.leon@gmail.com>
2021-05-11 21:52:17 +0800

    Remove timestamps from category

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -10,12 +10,15 @@ use Illuminate\Database\Eloquent\Model;
  * 
  * @property int $id
  * @property string $name
- * @property string $created_at
- * @property string $updated_at
  */
 class Category extends Model
 {
     use HasFactory;
+
+    /**
+     * Disable created_at, updated_at field
+     */
+    public $timestamps = false;
 
     public function posts()
     {
